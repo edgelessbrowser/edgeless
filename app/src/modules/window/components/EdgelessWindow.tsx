70374b2844a396624841685894e8c1826fc37095
rakibtg <rakibtg@gmail.com>
2024-09-15 01:23:04 +0200

    feat: add EdgelessWindow component for dynamic window sizing and tab focus management

---
+++ app/src/modules/window/components/EdgelessWindow.tsx
@@ -0,0 +1,39 @@
+import { onMount } from "solid-js";
+import { JSX } from "solid-js/jsx-runtime";
+import BrowserEvents from "../../../utils/browserEvents";
+import EdgelessWindowState from "../store/EdgelessWindowState";
+import ViewPanelState from "../../webview-panels/store/ViewPanelState";
+
+interface EdgelessWindowProps {
+  children: JSX.Element;
+}
+
+function EdgelessWindow({ children }: EdgelessWindowProps) {
+  onMount(() => {
+    BrowserEvents.on("baseWindow:sizeUpdate", (data) => {
+      console.log("Received Bounds:", data);
+      EdgelessWindowState.setBaseWindowSize({
+        width: data.width + "px",
+        height: data.height + "px",
+      });
+    });
+
+    BrowserEvents.on("tab:focused", (data) => {
+      ViewPanelState.setFocusedTab(data.name);
+    });
+  });
+
+  return (
+    <div
+      style={{
+        width: EdgelessWindowState.baseWindowSize().width,
+        height: EdgelessWindowState.baseWindowSize().height,
+      }}
+      class="bg-slate-600 text-white flex flex-col overflow-hidden"
+    >
+      {children}
+    </div>
+  );
+}
+
+export default EdgelessWindow;
