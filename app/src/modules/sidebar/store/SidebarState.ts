1b4963a87bbe3f19ce547691b2922fd9319248a0
rakibtg <rakibtg@gmail.com>
2024-09-15 01:21:56 +0200

    feat: add SidebarState for managing sidebar visibility and toggle functionality

---
+++ app/src/modules/sidebar/store/SidebarState.ts
@@ -0,0 +1,10 @@
+import { createSignal, createRoot } from "solid-js";
+
+function SidebarState() {
+  const [viewSidebar, setViewSidebar] = createSignal(true);
+  const toggleSidebar = () => setViewSidebar(!viewSidebar());
+
+  return { viewSidebar, toggleSidebar };
+}
+
+export default createRoot(SidebarState);
