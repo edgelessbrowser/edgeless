886f3ff9a37ba0be409fbe77e4c28802fc796ca7
rakibtg <rakibtg@gmail.com>
2024-09-21 00:30:46 +0200

    add DefaultPage component for initial page layout

---
+++ app/src/modules/pages/components/DefaultPage.tsx
@@ -0,0 +1,14 @@
+export default function DefaultPage() {
+  return (
+    <div
+      class={`
+      w-full h-screen flex justify-center items-center flex-col 
+      text-sm text-white bg-slate-700
+    `}
+    >
+      <h1 class="font-bold text-2xl text-slate-800/60 select-none">
+        Edgeless Browser
+      </h1>
+    </div>
+  );
+}
