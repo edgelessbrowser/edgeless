54bf669af1e734fa6f5c067d6b7867906ec391ac
rakibtg <rakibtg@gmail.com>
2024-09-24 23:28:53 +0200

    Replace div with Box component in DefaultPage for improved layout

--- app/src/modules/pages/components/DefaultPage.tsx
+++ app/src/modules/pages/components/DefaultPage.tsx
@@ -1,6 +1,8 @@
+import Box from "../../ui/components/Box";
+
 export default function DefaultPage() {
   return (
-    <div
+    <Box
       class={`
       w-full h-screen flex justify-center items-center flex-col 
       text-sm text-white bg-slate-700
@@ -9,6 +11,6 @@ export default function DefaultPage() {
       <h1 class="font-bold text-3xl text-slate-800/60 select-none">
         Edgeless Browser
       </h1>
-    </div>
+    </Box>
   );
 }
