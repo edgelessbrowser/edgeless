ec01b72cbe32bc0f6d6b6e6f609f04008570b720
rakibtg <rakibtg@gmail.com>
2024-09-26 19:10:13 +0200

    Enhance ToolbarButton to support additional class names for better styling flexibility

--- app/src/modules/toolbar/components/ToolbarButton.tsx
+++ app/src/modules/toolbar/components/ToolbarButton.tsx
@@ -8,8 +8,8 @@ export default function ToolbarButton(props: ButtonProps) {
   return (
     <button
       title="Toggle Sidebar"
-      class="p-1 win-no-drag hover:bg-slate-700/60 active:bg-slate-700 focus:bg-slate-700 rounded mt-1 transition-[background-color,color] duration-200 text-slate-300/90"
       {...props}
+      class={`p-1 win-no-drag hover:bg-slate-700/60 active:bg-slate-700 focus:bg-slate-700 rounded mt-1 transition-[background-color,color] duration-200 text-slate-300/90 ${props.class}`}
     >
       {props.children}
     </button>
