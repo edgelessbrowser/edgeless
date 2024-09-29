4d4c80be829cca93505592286a028ba6b4f9ff2c
rakibtg <rakibtg@gmail.com>
2024-09-29 23:13:42 +0200

    Add removeListener method to browserEvents for improved event management

--- app/src/utils/browserEvents.ts
+++ app/src/utils/browserEvents.ts
@@ -3,6 +3,7 @@ declare global {
     ___ebp___: {
       send: (channel: string, data: any) => void;
       on: (channel: string, func: (...args: any[]) => void) => void;
+      removeListener: (channel: string, func: (...args: any[]) => void) => void;
     };
   }
 }
