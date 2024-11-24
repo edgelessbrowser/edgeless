237dcd6b053cb4128d173babbf30e4db811c63e5
rakibtg <rakibtg@gmail.com>
2024-11-24 17:23:45 +0100

    add baseWindow function for creating a customizable Electron window

---
+++ src/main/windows/baseWindow.ts
@@ -0,0 +1,21 @@
+import { BaseWindow } from 'electron'
+
+export const baseWindow = (): BaseWindow => {
+  return new BaseWindow({
+    width: 1200,
+    height: 800,
+    // transparent: true,
+    frame: false,
+    show: true,
+    // titleBarStyle: 'hidden',
+    titleBarOverlay: false,
+    backgroundColor: '#475569',
+    // center: false,
+    roundedCorners: true,
+    useContentSize: true,
+    trafficLightPosition: {
+      x: 15,
+      y: 12
+    }
+  })
+}
