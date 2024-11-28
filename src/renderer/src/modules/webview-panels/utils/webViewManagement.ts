664a35e462edb784aa25f2e80ee36afc4023a2f7
rakibtg <rakibtg@gmail.com>
2024-11-28 23:25:49 +0100

    refactor webViewManagement to clean up addNewTab function and enable tab removal functionality

--- src/renderer/src/modules/webview-panels/utils/webViewManagement.ts
+++ src/renderer/src/modules/webview-panels/utils/webViewManagement.ts
@@ -2,11 +2,12 @@ import ViewPanelState from '../store/ViewPanelState'
 import BrowserEvents from '../../../utils/browserEvents'
 
 export async function addNewTab() {
-  const panel = await BrowserEvents.invoke('TAB:CREATE')
-  console.log('panel:', panel)
+  // const panelBounds = {}
+  // const panel = await BrowserEvents.invoke('TAB:CREATE')
+  // console.log('panel:', panel)
 }
 
 export function removeTab(id: string) {
   ViewPanelState.removePanel(id)
-  // BrowserEvents.send('TAB:REMOVE', id)
+  BrowserEvents.send('TAB:REMOVE', id)
 }
