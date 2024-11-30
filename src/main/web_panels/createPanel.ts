5338b23992b54f0720da0bdb912abff1b7562d29
rakibtg <rakibtg@gmail.com>
2024-11-30 09:28:17 +0100

    update electron version and modify user agent string in createPanel function

--- src/main/web_panels/createPanel.ts
+++ src/main/web_panels/createPanel.ts
@@ -103,7 +103,7 @@ export const createPanel = (props: PanelInterface) => {
 
   panel.setBackgroundColor('#475569')
   panel.webContents.userAgent =
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130 Safari/537.36 EdgelessBrowser/1.0'
 
   // contextMenu({
   //   window: panel.webContents,
