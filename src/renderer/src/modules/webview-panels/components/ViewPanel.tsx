38e8cbedc918a729a0d0e797e7f31056b9964a46
rakibtg <rakibtg@gmail.com>
2024-12-20 17:09:42 +0100

    wrap ViewPanel content with PanelBorder component for dynamic styling

--- src/renderer/src/modules/webview-panels/components/ViewPanel.tsx
+++ src/renderer/src/modules/webview-panels/components/ViewPanel.tsx
@@ -1,6 +1,7 @@
 import BrowserEvents from '../../../utils/browserEvents'
 import ViewPanelState, { PanelInterface } from '../store/ViewPanelState'
 import { createSignal, onMount, onCleanup, createEffect } from 'solid-js'
+import PanelBorder from './PanelBorder'
 
 function ViewPanel(props: { panel: PanelInterface }) {
   let panelContainerRef: HTMLDivElement | undefined
@@ -48,22 +49,24 @@ function ViewPanel(props: { panel: PanelInterface }) {
   })
 
   return (
-    <div
-      class="h-full relative"
-      style={{
-        width: widthPercent()
-      }}
-    >
+    <PanelBorder active={props.panel.isVisible || false}>
       <div
-        class="bg-slate-700 p-4 min-w-52 w-full h-full rounded-md"
-        ref={(el) => (panelContainerRef = el)}
-      />
-      {isFocusedPanel() && (
-        <div class="absolute top-0 left-0 right-0 -mt-1.5 px-0.5 select-none">
-          <div class="bg-slate-400/80 hover:bg-slate-400 h-1 rounded"></div>
-        </div>
-      )}
-    </div>
+        class="h-full relative"
+        style={{
+          width: widthPercent()
+        }}
+      >
+        <div
+          class="bg-slate-700 p-4 min-w-52 w-full h-full rounded-md"
+          ref={(el) => (panelContainerRef = el)}
+        />
+        {isFocusedPanel() && (
+          <div class="absolute top-0 left-0 right-0 -mt-1.5 px-0.5 select-none">
+            <div class="bg-slate-400/80 hover:bg-slate-400 h-1 rounded"></div>
+          </div>
+        )}
+      </div>
+    </PanelBorder>
   )
 }
 
