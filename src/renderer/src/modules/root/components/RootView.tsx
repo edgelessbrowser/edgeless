913836e3aa2384dcaf2a09f4ad5685dd2e08df52
rakibtg <rakibtg@gmail.com>
2025-01-02 06:10:26 +0100

    enhance RootView component to handle panel creation via events

--- src/renderer/src/modules/root/components/RootView.tsx
+++ src/renderer/src/modules/root/components/RootView.tsx
@@ -1,13 +1,14 @@
 import Box from '../../ui/components/Box'
+import RootState from '../store/RootState'
+import useEvents from '../../../hooks/useEvents'
 import Sidebar from '../../sidebar/components/Sidebar'
 import WindowToolbar from '../../toolbar/components/WindowToolbar'
 import EdgelessWindow from '../../window/components/EdgelessWindow'
-import ViewPanelContainer from '../../webview-panels/components/ViewPanelContainer'
-import useEvents from '../../../hooks/useEvents'
-import RootState from '../store/RootState'
 import { Architecture, OSName, Theme } from '@renderer/shared/types'
 import ToolbarState from '@renderer/modules/toolbar/store/ToolbarState'
 import SidebarState from '@renderer/modules/sidebar/store/SidebarState'
+import ViewPanelState from '@renderer/modules/webview-panels/store/ViewPanelState'
+import ViewPanelContainer from '../../webview-panels/components/ViewPanelContainer'
 
 export default function RootView() {
   useEvents({
@@ -23,6 +24,16 @@ export default function RootView() {
       RootState.setOsName(OSName[data.osName as keyof typeof OSName])
       RootState.setTheme(Theme[data.systemTheme as keyof typeof Theme])
       RootState.setArchitecture(Architecture[data.architecture as keyof typeof Architecture])
+    }
+  })
+
+  useEvents({
+    invoke: true,
+    channel: 'PANEL:GET_ALL',
+    callback: (data) => {
+      if (data.length === 0) {
+        ViewPanelState.addPanel()
+      }
     }
   })
 
