bc54314709bece68df0754ae846c821f5b0fe8d5
rakibtg <rakibtg@gmail.com>
2025-01-02 06:10:47 +0100

    enhance ToolbarState to toggle visibility with OS-specific events

--- src/renderer/src/modules/toolbar/store/ToolbarState.ts
+++ src/renderer/src/modules/toolbar/store/ToolbarState.ts
@@ -1,10 +1,23 @@
-import { createSignal, createRoot } from "solid-js";
+import { createSignal, createRoot } from 'solid-js'
+import BrowserEvents from '@renderer/utils/browserEvents'
+import RootState from '@renderer/modules/root/store/RootState'
+import { OSName } from '@renderer/shared/types'
 
 function ToolbarState() {
-  const [viewToolbar, setViewToolbar] = createSignal(true);
-  const toggleToolbar = () => setViewToolbar(!viewToolbar());
+  const [viewToolbar, setViewToolbar] = createSignal(true)
+  const toggleToolbar = () => {
+    const nextToolbarState = !viewToolbar()
 
-  return { viewToolbar, toggleToolbar };
+    if (RootState.osName() === OSName.Mac) {
+      BrowserEvents.send('baseWindow:toggleTrafficLights', {
+        nextToolbarState
+      })
+    }
+
+    setViewToolbar(nextToolbarState)
+  }
+
+  return { viewToolbar, toggleToolbar }
 }
 
-export default createRoot(ToolbarState);
+export default createRoot(ToolbarState)
